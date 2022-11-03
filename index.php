0b8b2d7d051043d6f3aa7b8d48dfe4cbb4cfddad
777dan <danya.nikitin7@gmail.com>
2022-11-03 18:33:26 +0200

    v 1.0

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php ob_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include "array_library.php";
+
+    echo "<table>";
+    echo "<tr><th>Режиссёр</th><th>Фильмы</th><th>Год выпуска</th></tr>";
+    function cmp_producer($a, $b)
+    {
+        return ($a['producer'] <=> $b['producer']);
+    }
+    function cmp_name($a, $b)
+    {
+        return ($a['name'] <=> $b['name']);
+    }
+    function cmp_year($a, $b)
+    {
+        return ($a['year'] <=> $b['year']);
+    }
+    function output($value1, $key1)
+    {
+        foreach ($value1 as $key2 => $value2) {
+            echo "<tr>";
+            if (!is_array($value2)) {
+                echo "<td rowspan='4'>$value2</td>\n";
+            }
+            if (is_array($value2)) {
+                if (isset($_GET['Radio2'])){
+                    usort($value2, "cmp_year");
+                }
+                if (isset($_GET['Radio3'])){
+                    usort($value2, "cmp_name");
+                }
+                foreach ($value2 as $key3 => $value3) {
+                    foreach ($value3 as $key4 => $value4) {
+                        echo "<td>$value4</td>\n";
+                    }
+                    echo "</tr>";
+                }
+                echo "\n";
+            }
+        }
+    }
+    if (isset($_GET['Submit'])) {
+        $seach_result1 = array_flip(search($films, $_GET['Search']));
+        $seach_result2 = array_intersect_key($films, $seach_result1);
+        if (isset($_GET['Radio1'])){
+            usort($seach_result2, "cmp_producer");
+        }
+        array_walk($seach_result2, "output");
+    }
+    echo "</table>\n";
+
+    function search($films, $data)
+    {
+        $result = array();
+        foreach ($films as $key1 => $value1) {
+            foreach ($value1 as $key2 => $value2) {
+                // if (!is_array($value2)) {
+                //     print "Введите название книги!";
+                //     // if (stristr($value2, $data)) {
+                //     //     $result[] = $key1;
+                //     // }
+                // }
+                if (is_array($value2)) {
+                    foreach ($value2 as $key3 => $value3) {
+                        foreach ($value3 as $key4 => $value4) {
+                            if (strstr($value3['name'], $data)) {
+                                $result[] = $key1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (count($result) == 0) {
+            print "По вашему запросу ничего не найдено(";
+        }
+        return array_unique($result);
+    }
+    if (isset($_GET['Clear'])) {
+        // unset($$_GET);
+        ob_end_clean();
+        header("Location:" . $_SERVER['PHP_SELF']);
+        exit;
+    }
+    ?>
+    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="GET">
+        <input type="text" name="Search" size="20"><br>
+        <input type="submit" name="Submit" size="50"><br>
+        <input type="submit" name="Clear" size="50" value="Заново"><br>
+        <input type="radio" id="radio1" name="Radio1"><label for="radio1">Сортировка по ФИ автора</label><br>
+        <input type="radio" id="radio2" name="Radio1"><label for="radio2">Сортировка по году выпуска</label><br>
+        <input type="radio" id="radio3" name="Radio1"><label for="radio3">Сортировка по названию фильма</label><br>
+    </form>
+</body>
+
+</html>
