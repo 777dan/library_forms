079f924cc3e80ccf7ac3e551264f5322b9ad3626
777dan <danya.nikitin7@gmail.com>
2022-11-08 11:20:05 +0200

    v 5.0

--- index.php
+++ index.php
@@ -36,13 +36,16 @@
         }
         echo "</tr>";
     }
-    if (isset($_GET['Submit'])) {
+    if (isset($_GET['Submit2'])) {
         if ($_GET['sort'] === "producer")
             usort($films, "cmp_producer");
         if ($_GET['sort'] === "year")
             usort($films, "cmp_year");
         if ($_GET['sort'] === "name")
             usort($films, "cmp_name");
+        array_walk($films, "output");
+    }
+    if (isset($_GET['Submit'])) {
         array_walk($films, "output");
     }
     echo "</table>\n";
@@ -69,14 +72,16 @@
         exit;
     }
     ?>
-    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="GET">
+    <form id="form1" action="<?= $_SERVER['PHP_SELF'] ?>" method="GET">
         <input type="text" name="Search" size="20" placeholder="Название фильма"><br>
         <input type="submit" name="Submit" size="50"><br>
         <input type="submit" name="Clear" size="50" value="Заново"><br>
-        <div id="radioDiv"><input type="radio" id="radio1" name="sort" value="producer" checked><label>Сортировка по ФИ автора</label><br>
+    </form>
+    <form id="form2" action="<?= $_SERVER['PHP_SELF'] ?>" method="GET">
+        <input type="radio" id="radio1" name="sort" value="producer" checked><label>Сортировка по ФИ автора</label><br>
         <input type="radio" id="radio2" name="sort" value="year"><label for="radio2">Сортировка по году выпуска</label><br>
-        <input type="radio" id="radio3" name="sort" value="name"><label for="radio3">Сортировка по названию фильма</label>
-        </div><br>
+        <input type="radio" id="radio3" name="sort" value="name"><label for="radio3">Сортировка по названию фильма</label><br>
+        <input type="submit" name="Submit2" size="50" value="Сортировка"><br>
     </form>
 </body>
 
